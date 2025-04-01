236a384bf56371e893be3feb26a03a790c32ff7d
kkuk-yang <yangyk7364@gmail.com>
2025-04-01 20:02:27 +0900

    인게임 작업 중

--- KDT2Framework/Include/Object/PlayerInGameOtherObject.cpp
+++ KDT2Framework/Include/Object/PlayerInGameOtherObject.cpp
@@ -55,43 +55,51 @@ void CPlayerInGameOtherObject::ProcessMessage(const RecvMessage& msg)
 	case (int)ServerMessage::MSG_TAKEN_DAMAGE:
 	{
 		auto info = CMultiplayManager::GetInst()->GetPlayerInfoValueById(GetNetID());
-		// mini HP 연결?
+		if (info.id == mNetID)
+		{
+			// mini HP 연결?
 
+		}
 		break;
 	}
 	case (int)ServerMessage::MSG_PLAYER_DEAD:
 	{
 		auto info = CMultiplayManager::GetInst()->GetPlayerInfoValueById(GetNetID());
-
-		// dead sign
-		mDeadSign->SetRelativePos(FVector3D(0.0f, 0.0f, -0.1f));
-		mDeadSign->SetEnable(info.isDeadInGame);
-
+		if (info.id == mNetID)
+		{
+			// dead sign
+			mDeadSign->SetRelativePos(FVector3D(0.0f, 0.0f, -0.1f));
+			mDeadSign->SetEnable(info.isDeadInGame);
+		}
 		break;
 	}
 	case (int)ServerMessage::MSG_PLAYER_DISTANCE:
-	{	
+	{
 		auto info = CMultiplayManager::GetInst()->GetPlayerInfoValueById(GetNetID());
-		// 현재 타게팅된 플레이어를 기준으로 상대적인 화면 위치를 지정해야 함.
-		float otherDist = info.distance;
-
-		auto inGameScene = dynamic_cast<CSceneInGame*>(mScene);
-		auto myPlayerObject = inGameScene->GetMyPlayerObject();
-		auto myPlayerStat = dynamic_cast<IPlayerStatController*>(myPlayerObject);
-		float myDist = myPlayerStat->GetPlayDistance();
-		float gapBaseOnTarget = myDist - otherDist; // 타겟(나)이 앞에있다면, +가 나옴.
-		FVector3D worldPos = FVector3D(myPlayerObject->GetWorldPosition().x - gapBaseOnTarget, info.height, 0.0f);
-		SetMovePlayer(worldPos);
+		if (info.id == mNetID)
+		{
+			// 현재 타게팅된 플레이어를 기준으로 상대적인 화면 위치를 지정해야 함.
+			float otherDist = info.distance;
+			auto inGameScene = dynamic_cast<CSceneInGame*>(mScene);
+			auto myPlayerObject = inGameScene->GetMyPlayerObject();
+			auto myPlayerStat = dynamic_cast<IPlayerStatController*>(myPlayerObject);
+			float myDist = myPlayerStat->GetPlayDistance();
+			float gapBaseOnTarget = myDist - otherDist; // 타겟(나)이 앞에있다면, +가 나옴.
+			FVector3D worldPos = FVector3D(myPlayerObject->GetWorldPosition().x - gapBaseOnTarget, info.height, 0.0f);
+			SetMovePlayer(worldPos);
+		}
 		break;
 	}
 	case (int)ServerMessage::MSG_PLAYER_HEIGHT:
 	{
 		auto info = CMultiplayManager::GetInst()->GetPlayerInfoValueById(GetNetID());
-		// 현재 타게팅된 플레이어를 기준으로 상대적인 화면 위치를 지정해야 함.
-		float otherHeight = info.height;
-		FVector3D worldPos = GetWorldPosition();
-		worldPos.y = otherHeight;
-		SetMovePlayer(worldPos);
+		if (info.id == mNetID)
+		{
+			float otherHeight = info.height;
+			FVector3D worldPos = GetWorldPosition();
+			worldPos.y = otherHeight;
+			SetMovePlayer(worldPos);
+		}
 		break;
 	}
 	default:
