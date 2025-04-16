1172ac5dc160929da4ff92f32686ec0805ead31b
didrnrgus <yangyk7364@gmail.com>
2025-04-16 19:10:45 +0900

    주석 수정

--- KDT2Framework/Include/Interface/ISceneNetworkController.h
+++ KDT2Framework/Include/Interface/ISceneNetworkController.h
@@ -8,24 +8,25 @@ protected:
 
     // CScene 또는  상속받은 클래스의 Update() 안에서 호출 되어야 함.
     virtual void ProcessMessage() = 0;
-    /*  void ProcessMessage()
-		{
-			// 메시지 큐에서 메시지 뽑는 역할.
-			RecvMessage msg;
-			if (CNetworkManager::GetInst()->PollMessage(msg))
-			{
-				DistributeMessage(msg);
+    /*  
+        void ProcessMessage()
+        {
+	        RecvMessage msg;
+	        while (CNetworkManager::GetInst()->PollMessage(msg))
+            {
+		        DistributeMessage(msg);
 
-				// 순서: 커넥트 -> 방장누구? -> 기존인원 리스트
-				switch (msg.msgType)
-				{
-				case (int)ServerMessage::Type::MSG_JOIN:
-				{
-					break;
-				}
-				}
-			}
-		}    */
+                switch (msg.msgType)
+                {
+                case (int)ServerMessage::Type::MSG_JOIN:
+                {
+                    break;
+                }
+                }
+            }
+
+        }
+    */
 
     // ProcessMessage() 내에서 메시지 가져온 다음에 호출 함.
     virtual void DistributeMessage(const struct RecvMessage& msg) = 0;
